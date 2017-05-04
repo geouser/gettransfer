5388ca05a2b770036ef2b07e8accdb99a0bab2e9
rusyi <olehrusy@gmail.com>
2017-05-04 21:52:40 +0300

    ds

--- js/main.js
+++ js/main.js
@@ -86,6 +86,15 @@ jQuery(document).ready(function($) {
     });
 
 
+    $('.offer--slider').slick({
+        autoplay: true,
+        fade: true,
+        speed: 900,
+        autoplaySpeed: 7000,
+        dots: false,
+        arrows: false
+    });
+
 
     /*---------------------------
                                   Fancybox
@@ -199,5 +208,42 @@ jQuery(document).ready(function($) {
     if ( exist( '#map_canvas' ) ) {
         googleMap_initialize();
     }
+
+
+    /* Clock */
+    var $dOut = $('#date'),
+        $hOut = $('#hours'),
+        $mOut = $('#minutes'),
+        $sOut = $('#seconds');
+
+    function update(){
+      var date = new Date();
+      
+      var ampm = date.getHours() < 12
+                 ? 'AM'
+                 : 'PM';
+      
+      var hours = date.getHours() == 0
+                  ? 12
+                  : date.getHours() > 12
+                    ? date.getHours() - 12
+                    : date.getHours();
+      
+      var minutes = date.getMinutes() < 10 
+                    ? '0' + date.getMinutes() 
+                    : date.getMinutes();
+      
+      var seconds = date.getSeconds() < 10 
+                    ? '0' + date.getSeconds() 
+                    : date.getSeconds();
+      
+
+      $hOut.text(hours);
+      $mOut.text(minutes);
+      $sOut.text(seconds);
+    } 
+
+    update();
+    window.setInterval(update, 1000);
 
 }); // end file
